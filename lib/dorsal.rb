6e540e951aa80646b5a6f929b190591119ce3316
= <=>
2023-06-03 14:34:16 +0200

    base for gem

---
+++ lib/dorsal.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dorsal/version"
+
+module Dorsal
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dorsal/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Dorsal
+  VERSION = "0.1.0"
+end
